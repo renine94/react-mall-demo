b98025ecbb3379e147baa1d8d65907870fae60f3
kyle <jg.kang@fitpet.co.kr>
2023-04-16 21:15:22 +0900

    [React] Home 페이지에서 axios 사용하여 데이터 가져오기

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,6 +8,7 @@ const ProductCard = (props) => {
             <img src={`https://codingapple1.github.io/shop/shoes${props.i}.jpg`}  width="80%" alt={`shose${props.i}`} />
             <h4>{props.shoes.title}</h4>
             <p>{props.shoes.content}</p>
+            <p>{props.shoes.price}</p>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import ProductCard from '../components/ProductCard';
 
-import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 
 const Home = (props) => {
-  const navigate = useNavigate()
-  let shoes = props.shoes;
+  let [shoes, setShoes] = useState(props.shoes);
+  let [clickCount, setClickCount] = useState(2);
 
   let cardList = shoes.map((a, i) => {
       return (
@@ -13,11 +14,27 @@ const Home = (props) => {
           shoes={shoes[i]} 
           i={i+1} 
           key={i} 
-          // onClick={() => { navigate(`/detail/${i+1}`) }}
-          // href="/asdf"
         />
       )
     })
+
+  let addShoes = async () => {
+    await axios.get(`https://codingapple1.github.io/shop/data${clickCount}.json`)
+          .then((res) => {
+            console.log(`https://codingapple1.github.io/shop/data${clickCount}.json`)
+            if (clickCount < 3) {
+              setClickCount(++clickCount);
+            } else {
+              console.log('클릭횟수 3회 초과');
+              return
+            }
+            setShoes([...shoes, ...res.data]);
+            // remove Loading UI
+          })
+          .catch(() => {
+            // remove Loading UI
+          })
+  }
 
   return (
     <>
@@ -27,6 +44,7 @@ const Home = (props) => {
           {cardList}
         </div>
       </div>
+      <button onClick={() => { addShoes() }}>버튼</button>
     </>
   );
 };
