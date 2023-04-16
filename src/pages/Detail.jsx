5579201723dde4ef7026f73948c7193b8f4b506b
kyle <jg.kang@fitpet.co.kr>
2023-04-16 20:17:03 +0900

    [React] Detail 페이지에 useEffect LifeCycle Hook 적용하는 여러가지 방법

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import styled from "styled-components";
@@ -12,6 +13,36 @@ let Btn = styled.button`
 const Detail = (props) => {
   let { id } = useParams();
   let shoe = props.shoes.find(item => item.id === id-1);
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let [num, setNum] = useState('');
+
+  // Lifecycle Hook (렌더링이 다 되고나서, 실행된다.)
+  useEffect(() => {
+    let b = setTimeout(() => {setAlert(false);}, 5000)
+    console.log(1);
+
+     // useEffect 안에 있는 코드가 실행되기전에 실행하는 코드
+    return () => {
+      console.log('useEffect 가 실행되기 전에 실행되는 코드')
+      clearTimeout(b);
+    }
+  }, [])  // mount 될때만 1회만 실행되도록 [] (vue.js) 의 watch와 비슷하다.
+  
+  // useEffect(() => {})       // 재렌더링마다 코드 실행하고싶을때,
+  // useEffect(() => {}, [])   // mount시 1회 코드 실행하고 싶으면
+  // useEffect(() => {
+  //   return () => {
+  //     // unmount시 1회 코드실행하고 싶을때,
+  //   }
+  // }, [])
+
+  useEffect(() => {
+    if (isNaN(num) === true) {
+      console.log("숫자만 입력하세요.")
+    }
+  }, [num])
+
 
   // 상품이 없으면
   if (!shoe) {
@@ -22,13 +53,23 @@ const Detail = (props) => {
 
   return (
     <div className="container">
-      {/* <Btn bg="yellow">버튼</Btn>
-      <Btn bg="blue">버튼</Btn> */}
+      {
+        alert === true
+        ? <div className="alert alert-warning">
+            5초 이내 구매시 할인
+          </div>
+        : null
+      }
+
+      { count }
+      <Btn bg="yellow" onClick={() => { setCount(count++) }}>버튼</Btn>
+
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${id}.jpg`} width="100%" alt={id}/>
         </div>
         <div className="col-md-6">
+          <input type="text" onChange={(e) => setNum(e.target.value) }></input>
           <h4 className="pt-5">{shoe.title}</h4>
           <p>{shoe.content}</p>
           <p>{shoe.price}원</p>
