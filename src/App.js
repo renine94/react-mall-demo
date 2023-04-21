61ffc14b4742560bea035d8a33fbb2aaea610d75
kyle <jg.kang@fitpet.co.kr>
2023-04-21 13:27:19 +0900

    [React] Components Lazy Loading & Suspense 로 로딩중일떄 보여줄 UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, lazy, Suspense, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import MyNavbar from "./layouts/MyNavbar";
 
 import Home from "./pages/Home";
-import Detail from "./pages/Detail";
+// import Detail from "./pages/Detail";
 import About from "./pages/About";
+
 import AboutMember from "./pages/AboutMember";
 import AboutLocation from "./pages/AboutLocation";
 import Cart from "./pages/Cart";
@@ -17,6 +18,7 @@ import data from './data';
 // state 보관함
 export let Context1 = createContext()
 
+const Detail = lazy(() => import("./pages/Detail"));
 
 function App() {
 
@@ -34,19 +36,21 @@ function App() {
       <MyNavbar></MyNavbar>
 
       {/* Router */}
-      <Routes>
-        <Route path="/" element={<Home shoes={shoes} />} />
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-        <Route path="/cart" element={<Cart />} />
+      <Suspense fallback={<div>로딩중..</div>}>
+        <Routes>
+          <Route path="/" element={<Home shoes={shoes} />} />
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+          <Route path="/cart" element={<Cart />} />
 
-        <Route path="/about" element={<About />}>
-          {/* Outlet 으로 보여줄 위치 뚫어줘야함 */}
-          <Route path="member" element={<AboutMember />} />
-          <Route path="location" element={<AboutLocation />} />
-        </Route>
+          <Route path="/about" element={<About />}>
+            {/* Outlet 으로 보여줄 위치 뚫어줘야함 */}
+            <Route path="member" element={<AboutMember />} />
+            <Route path="location" element={<AboutLocation />} />
+          </Route>
 
-        <Route path="*" element={<h2>404 Not Found</h2>} />
-      </Routes>
+          <Route path="*" element={<h2>404 Not Found</h2>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
